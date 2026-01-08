c80138d4c4906b543724747702d8a3b0bccd7341
Raj Kumar Reddy <tech.razzukwn@gmail.com>
2026-01-08 16:49:51 +0530

    all

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./**/*.{tsx,ts}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
